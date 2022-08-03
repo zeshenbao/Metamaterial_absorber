eca13f6049e9a0c27acd1a87f1f8facb709e3c5a
zeshen <zeshen@hotmail.se>
2022-08-03 09:26:02 +0200

    Added more commands

--- current/own_code/VNA_code/VNA_notes.py
+++ current/own_code/VNA_code/VNA_notes.py
@@ -7,4 +7,24 @@ SCPI commands good to know:
 SENS - used for data retrival
 CALC - used for data processing
 
+
+
+znb commands:
+
+
+znb.instrument_status_checking = True  # Error check after each command, can be True or False
+
+znb.clear_status()  # Clear status register
+
+znb.close() # close session
+
+znb.query_str('*IDN?') # check VNA id
+
+RsInstrument.assert_minimum_version('1.50.0') # check version
+
+resource = 'TCPIP0::10.33.38.33::INSTR'  # VISA resource string for the device
+
+znb = RsInstrument(resource, True, True, "SelectVisa='rs'") # select rs VISA and other configs.
+
+
 """
