ce13af61834b5ded930526d060a7ed6d5bdc5ab4
Zeshen <zeshen@kth.se>
2022-08-10 10:23:46 +0200

    old file as hilbert_dog_leg_V3. New file as gen_MeMAb, test_dict tests dictionary methods

--- current/own_code/MeMAb_code/test_dict.py
+++ current/own_code/MeMAb_code/test_dict.py
@@ -1,7 +1,12 @@
 
-
-dict1 = {"Artiiii", "Baldi", "Crevi"}
+"""
+dict1 = {"Artiiii":1, "Baldi":2, "Crevi":3}
 #print(type(dict1.keys()))
 
 for i in dict1:
     print(i[2:])
+
+    
+"""
+
+dict1 = {[0,1]:1, [2,3]:2, [4,6]:3}
